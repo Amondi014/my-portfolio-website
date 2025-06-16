a71f60dfd8ec5d429f32558652a0ad30a0ca080e
Edith Amondi <eamondi014@gmail.com>
2025-06-17 00:01:53 +0300

    chore: initial project setup

---
+++ src/data/socials.ts
@@ -0,0 +1,30 @@
+export interface Social {
+    name: string;
+    link: string;
+}
+
+const socials: Social[] = [
+    {
+      name: 'Github',
+      link: 'https://github.com/Amondi014',
+    },
+    {
+      name: 'Linked In',
+      link: 'https://www.linkedin.com/in/edith-amondi-4160622b1/',
+    },
+    
+    {
+      name: 'Medium',
+      link: 'https://medium.com/@sidneykaguli',
+    },
+    {
+      name: 'Wellfound',
+      link: 'https://angel.co/sidney-kaguli',
+    },
+    {
+      name: 'Resume',
+      link: 'https://docs.google.com/document/d/1Fg2qQOS2DUQ9nt2wWuEzuwG4JHrVEiY85zPv2G2S0nA/edit?usp=sharing',
+    },
+  ];
+  
+  export default socials;
